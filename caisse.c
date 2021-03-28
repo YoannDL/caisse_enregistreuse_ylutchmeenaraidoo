c478b524465dcf98edf8cfe5a7704361937bfcf0
YoannDL <79832529+YoannDL@users.noreply.github.com>
2021-03-28 10:53:10 +0400

    le programme conte maintenant le nobre de chaque billet ou piece a rendre v0.2

--- caisse.c
+++ caisse.c
@@ -4,22 +4,49 @@
 
 int main()
 {
-    double random_value;
+	double change;
+    double montant_a_payer;
+    //int cashValues;
+    double difference;
 
     srand ( time ( NULL));
 
-    random_value = (double)rand()/RAND_MAX*2000.0-1.0;//float in range 2000 to 1
+    montant_a_payer = (double)rand()/RAND_MAX*2000;//float in range 2000 to 1;
+    printf("Le montant a payez est:");
 
-    printf ( "%f\n", random_value);
+    printf ( "%f\n", montant_a_payer);
 
-    return 0;
     
-   double billet[]={2000,1000,500,200,100,50,25,20,10,5,1,0.5,0.2,0.05};
    int montant_client;
+   printf("veuillez inserer le montant pour payer svp:");
    scanf("%d", &montant_client);
-   if(montant_client<random_value)
+   change=montant_client-montant_a_payer;
+   if(change<0)
    {
-   	scanf( "Le montant inserer est inferieure au montant a payer, veuillez reinserer le montant svp",montant_client );
+   	printf( "Le montant inserer est inferieure au montant a payer, veuillez reinserer le montant svp:");
+   	scanf("%d",&montant_client );
+   	
+   	
    }
+   change=montant_client-montant_a_payer;
+   printf("%f\n",change);
+   
+   //function to calculate the change 
+   double cashValues[14] = {2000,1000,500,200,100,50,25,20,10,5,1,0.5,0.2,0.05};
+   
+   difference = change;
+   while (difference != 0) {
+	  int i; 
+      for(i=0; i<14; i++) {
+         if(cashValues[i] <= difference) {
+              difference -= cashValues[i];
+              printf("%f \n", cashValues[i]);
+              i=0;
+         }
+    }
+}
+   
     
+    
+   return 0 ;
 }
